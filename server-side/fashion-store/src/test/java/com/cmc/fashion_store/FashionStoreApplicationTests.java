544f8305cefd99a95fbfa1f62acf1a5b2a21f9e5
KienDoTri <kiendotri@gmail.com>
2025-10-15 17:11:16 +0700

    init

---
+++ server-side/fashion-store/src/test/java/com/cmc/fashion_store/FashionStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmc.fashion_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
