238bb36cef8770e78d1bc921c026269af86f7564
KienDoTri <kiendotri@gmail.com>
2025-10-24 13:15:17 +0700

    add base

--- fashion-store-vite/vite.config.js
+++ fashion-store-vite/vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server:{host: '0.0.0.0', // Lắng nghe trên tất cả các địa chỉ IP
-    port: 5173, // Chọn cổng muốn sử dụng
-    strictPort: true, // Đảm bảo Vite không thay đổi cổng
+  plugins: [react(), tailwindcss()],
+  base: './', // 👈 thêm dòng này — rất quan trọng khi deploy qua Nginx
+  server: {
+    host: '0.0.0.0', // Cho phép truy cập từ ngoài VM
+    port: 5173,
+    strictPort: true,
     cors: true,
-    
-  }
+  },
 })
