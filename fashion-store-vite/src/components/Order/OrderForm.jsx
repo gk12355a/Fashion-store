23f2efc25724a381c9a69e49ddba92f98734e84a
admin <admin@LAPTOP-SIVPSKVF>
2025-10-26 08:36:12 +0700

    update

--- fashion-store-vite/src/components/Order/OrderForm.jsx
+++ fashion-store-vite/src/components/Order/OrderForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import "../Form.css"; // <- ĐÃ XÓA
+
 import ReusableSearch from "../Common/ReusableSearch";
 import CartTable from "./CartTable";
 import { toast } from "react-toastify";
